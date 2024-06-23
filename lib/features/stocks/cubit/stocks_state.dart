2f6cda5fb579286aadb7ebbc4167cc54778c272f
Santiago Forero A <sanfo0620@gmail.com>
2024-06-23 12:45:29 -0500

    feat: Add companies list to StocksState

--- lib/features/stocks/cubit/stocks_state.dart
+++ lib/features/stocks/cubit/stocks_state.dart
@@ -3,19 +3,30 @@ part of 'stocks_cubit.dart';
 class StocksState extends Equatable {
   final Map<String, dynamic> stocksData;
   final bool hasError;
+  final List<Company> companies;
 
   const StocksState({
     this.stocksData = const {},
     this.hasError = false,
+    this.companies = const [],
   });
 
-  StocksState copyWith({Map<String, dynamic>? stocksData, bool? hasError}) {
+  StocksState copyWith({
+    List<Company>? companies,
+    Map<String, dynamic>? stocksData,
+    bool? hasError,
+  }) {
     return StocksState(
       stocksData: stocksData ?? this.stocksData,
       hasError: hasError ?? this.hasError,
+      companies: companies ?? this.companies,
     );
   }
 
   @override
-  List<Object> get props => [stocksData, hasError];
+  List<Object> get props => [
+        stocksData,
+        hasError,
+        companies,
+      ];
 }
