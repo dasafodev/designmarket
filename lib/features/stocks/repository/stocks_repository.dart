79be6a98c16d38795a46bc5a702946d4c1067a36
Santiago Forero A <sanfo0620@gmail.com>
2024-06-23 12:45:08 -0500

    feat: Add WebSocket connection and API calls to StocksRepository

--- lib/features/stocks/repository/stocks_repository.dart
+++ lib/features/stocks/repository/stocks_repository.dart
@@ -1,26 +1,89 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:desigmarket/features/stocks/models/company_model.dart';
+import 'package:desigmarket/features/stocks/models/quote_model.dart';
+import 'package:dio/dio.dart';
 import 'package:web_socket_channel/status.dart' as status;
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class StocksRepository {
   late WebSocketChannel channel;
+  late Dio dio;
+  late Stream broadcastStream;
   final String token;
+
+  final btcCompany = Company(
+    name: 'Bitcoin',
+    ticker: 'BINANCE:BTCUSDT',
+    logo: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+    country: 'US',
+    currency: 'USD',
+    exchange: 'Binance',
+    ipo: DateTime(2009, 1, 3),
+    marketCapitalization: 1.0,
+    phone: '1-800-555-5555',
+    weburl: 'https://bitcoin.org/',
+    finnhubIndustry: 'Cryptocurrency',
+    estimateCurrency: 'USD',
+    shareOutstanding: 1.0,
+    quote: Quote(
+      ticker: 'BINANCE:BTCUSDT',
+      currentPrice: 0.0,
+      highPrice: 0.0,
+      lowPrice: 0.0,
+      openPrice: 0.0,
+      previousClosePrice: 0.0,
+      timestamp: 0,
+    ),
+  );
+  static const stocks = [
+    'AAPL',
+    'GOOGL',
+    'AMZN',
+    'TSLA',
+    'BINANCE:BTCUSDT',
+  ];
 
   StocksRepository({required this.token}) {
     channel = WebSocketChannel.connect(
       Uri.parse('wss://ws.finnhub.io?token=$token'),
     );
-
-    channel.stream.listen(
-      (message) {
-        print('Message from server: $message');
-      },
-      onError: (error) {
-        print('WebSocket Error: $error');
-      },
-      onDone: () {
-        print('WebSocket closed');
-      },
+    dio = Dio(
+      BaseOptions(
+        baseUrl: 'https://finnhub.io/api/v1',
+        queryParameters: {'token': token},
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      ),
     );
+    broadcastStream = channel.stream.asBroadcastStream();
+    _subscribeToStocks();
+  }
+
+  Future<Company> getStockProfile(String symbol) async {
+    if (symbol == 'BINANCE:BTCUSDT') return btcCompany;
+    try {
+      final response =
+          await dio.get('/stock/profile2', queryParameters: {'symbol': symbol});
+      return Company.fromJson(response.data as Map<String, dynamic>);
+    } catch (e) {
+      log('Error fetching stock profile: $e');
+      rethrow;
+    }
+  }
+
+  Future<Quote> getStockQuote(String symbol) async {
+    if (symbol == 'BINANCE:BTCUSDT') return btcCompany.quote!;
+    try {
+      final response =
+          await dio.get('/quote', queryParameters: {'symbol': symbol});
+      return Quote.fromJson(response.data, ticker: symbol);
+    } catch (e) {
+      log('Error fetching stock quote: $e');
+      rethrow;
+    }
   }
 
   void subscribe(String symbol) {
@@ -28,7 +91,8 @@ class StocksRepository {
       'type': 'subscribe',
       'symbol': symbol,
     };
-    channel.sink.add(subscribeMessage);
+
+    channel.sink.add(jsonEncode(subscribeMessage));
   }
 
   void unsubscribe(String symbol) {
@@ -36,7 +100,14 @@ class StocksRepository {
       'type': 'unsubscribe',
       'symbol': symbol,
     };
-    channel.sink.add(unsubscribeMessage);
+
+    channel.sink.add(jsonEncode(unsubscribeMessage));
+  }
+
+  void _subscribeToStocks() {
+    for (final stock in stocks) {
+      subscribe(stock);
+    }
   }
 
   void dispose() {
